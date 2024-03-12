8e3cb6eee45d1a208292becfb2641d364ebf970c
jacreyespavy <163002258+jacreyespavy@users.noreply.github.com>
2024-03-12 11:20:14 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -16,12 +16,12 @@ def evaluate(human_labeled_file,pred_file):
     accuracy = accuracy_score(human_labeled_senti, pred_senti)
     macro_f1 = f1_score(human_labeled_senti, pred_senti, labels=senti_labels, average='macro')
     micro_f1 = f1_score(human_labeled_senti, pred_senti, labels=senti_labels, average='micro')
-    # 输出 overall accuracy, Macro-F1, Micro-F1
+    # Output overall accuracy, Macro-F1, Micro-F1
     print(f"accuracy_score: {accuracy}")
     print(f"macro: {macro_f1}")
     print(f"micro: {micro_f1}")
     result.extend([accuracy, macro_f1, micro_f1])
-    # 计算每个类别的precision, recall, F1
+    # Calculate the precision, recall, and F1 score for each category.
     precision = precision_score(human_labeled_senti, pred_senti, labels=senti_labels, average=None)
     recall = recall_score(human_labeled_senti, pred_senti, labels=senti_labels, average=None)
     f1 = f1_score(human_labeled_senti, pred_senti, labels=senti_labels, average=None)
@@ -32,19 +32,19 @@ def evaluate(human_labeled_file,pred_file):
     print("\t".join([str(item) for item in result]))
 
 
-#绘制混淆矩阵
+#Plot the confusion matrix.
 def draw_confusion_matrix(human_labeled_file,pred_file):
     human_labeled_data = pd.read_csv(human_labeled_file)
     human_labeled_senti = list(human_labeled_data['sentiment'])
     pred_data = pd.read_csv(pred_file)
     pred_senti = list(pred_data['sentiment'])
-    # 生成混淆矩阵
+    # Generate the confusion matrix.
     cm = confusion_matrix(human_labeled_senti, pred_senti)
-    # 计算百分比
+    # Calculate the percentage.
     cm_percentage = cm.astype('float') / len(pred_senti)
     sns.set(font_scale=2.2)
     plt.figure(figsize=(8, 7))
-    # 使用热力图绘制混淆矩阵
+    # Plot the confusion matrix using a heatmap.
     sns.heatmap(cm_percentage, annot=True, fmt=".1%", cmap="Blues", xticklabels=senti_labels, yticklabels=senti_labels, annot_kws={"size": 33})
     plt.xlabel("Predicted labels", fontsize=23)
     plt.ylabel("True labels", fontsize=23)
